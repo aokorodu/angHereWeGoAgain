3367bbffffe7fad5a55eb2cf56e4f6afebaa785d
Abraham Okorodudu <abraham_okorodudu@innovationstudio.vanguard>
2018-03-07 15:18:25 -0500

    Adding other graphs
    Added optomistic and pessimistic graph lines, moved stuff around a bit

--- src/app/ready-graph/ready-graph.component.ts
+++ src/app/ready-graph/ready-graph.component.ts
@@ -16,21 +16,27 @@ export class ReadyGraphComponent implements OnInit {
   stageWidth = 800;
   stageHeight = 800;
   margin = 20;
-  graphHeight = this.stageHeight - (2 * this.margin);
+  bottomMargin = 200;
+  graphHeight = this.stageHeight - this.margin - this.bottomMargin;
   graphWidth = this.stageWidth - (2 * this.margin);
+  graphOrigin = new PVector(this.margin, this.margin + this.graphHeight);
 
   vertLine: any;
   totalRows = 40;
-  totalCols = 40
+  totalCols = 40;
+
   points: number[];
+  optPoints: number[];
+  pessPoints: number[];
 
   maxVal = 0;
-
   interest = .05;
-  startVal = 100;
+  startVal = 50; // in thousands of dollars
 
   graphContainer: any;
   graphLine: any;
+  graphLine_Opt: any;
+  graphLine_Pess: any;
 
   constructor() { }
 
@@ -45,8 +51,12 @@ export class ReadyGraphComponent implements OnInit {
     this.initGraphContainer();
     this.initHashLines();
     this.initVertLine();
-    this.calcGraphPoints();
+    this.calcDefaultGraphPoints();
+    this.calcOptGraphPoints();
+    this.calcPessGraphPoints();
     this.plotGraphPoints();
+    this.plotOptGraphPoints();
+    this.plotPessGraphPoints();
     this.initTicker();
   }
 
@@ -74,39 +84,35 @@ export class ReadyGraphComponent implements OnInit {
     
   }
 
-  initAxes()
-  {
+  initAxes() {
+    let strokeWidth = 2;
     let sh = new createjs.Shape();
-    sh.graphics.setStrokeStyle(2);
+    sh.x = this.graphOrigin.x;
+    sh.y = this.graphOrigin.y;
+    sh.graphics.setStrokeStyle(strokeWidth);
     sh.graphics.beginStroke("#FFFFFF99");
-    sh.graphics.moveTo(this.margin, this.margin);
-    sh.graphics.lineTo(20, this.stageHeight - 20);
-    sh.graphics.lineTo(this.stageWidth - 20, this.stageHeight - 20);
+    sh.graphics.moveTo(0, 0);
+    sh.graphics.lineTo(0, -this.graphHeight);
     sh.graphics.endStroke();
-    sh.cache(0,0, this.stageWidth, this.stageHeight);
+    sh.graphics.setStrokeStyle(strokeWidth);
+    sh.graphics.beginStroke("#FFFFFF99");
+    sh.graphics.moveTo(0, 0);
+    sh.graphics.lineTo(this.graphWidth, 0);
+    sh.graphics.endStroke();
     this.stage.addChild(sh);
-  }
-
-  initGraphContainer() {
-    this.graphContainer = new createjs.Container();
-    this.graphContainer.x = this.margin;
-    this.graphContainer.y = this.stageHeight - this.margin;
-
-    this.stage.addChild(this.graphContainer);
   }
 
   initHashLines() {
     for ( let col = 0; col < this.totalCols; col++) {
       let sh = new createjs.Shape();
-      sh.graphics.setStrokeStyle(1);
+      sh.graphics.setStrokeStyle(0);
       sh.alpha = .5;
       sh.graphics.beginStroke('#FFFFFF');
       sh.graphics.moveTo(0, 0);
-      sh.graphics.lineTo(0, this.graphHeight);
+      sh.graphics.lineTo(0, -this.graphHeight);
       sh.graphics.endStroke();
-      sh.cache(0, -1, 2, this.graphHeight);
       sh.x = ((col + 1) * this.graphWidth / this.totalCols);
-      sh.y = -this.graphHeight;
+      sh.y = 0;
       this.graphContainer.addChild(sh);
     }
 
@@ -125,34 +131,67 @@ export class ReadyGraphComponent implements OnInit {
     }
   }
 
+  initGraphContainer() {
+    this.graphContainer = new createjs.Container();
+    this.graphContainer.x = this.graphOrigin.x;
+    this.graphContainer.y = this.graphOrigin.y;
+
+    this.stage.addChild(this.graphContainer);
+  }
+
   initVertLine() {
     this.vertLine = new createjs.Shape();
     this.vertLine.alpha = .4;
     this.vertLine.x = 100;
-    this.vertLine.y = this.margin;
+    this.vertLine.y = this.graphOrigin.y + 50;
     this.vertLine.graphics.setStrokeStyle(30);
     this.vertLine.graphics.beginStroke("#FFFFFF");
     this.vertLine.graphics.moveTo(0, 0);
-    this.vertLine.graphics.lineTo(0, this.graphHeight);
+    this.vertLine.graphics.lineTo(0, 50);
     this.vertLine.graphics.endStroke();
-    this.vertLine.cache(-1, 0, -1, this.graphHeight)
+    this.vertLine.cache(-1, 0, -1, 50)
     this.stage.addChild(this.vertLine);
   }
 
-  calcGraphPoints() {
+  drawGraphLines(){
+    this.calcDefaultGraphPoints();
+    this.calcOptGraphPoints();
+    this.calcPessGraphPoints();
+    this.plotGraphPoints();
+    this.plotOptGraphPoints();
+    this.plotPessGraphPoints();
+  }
 
+  calcDefaultGraphPoints() {
     // A = P (1 + r/n) ^ nt
-    // A = startVal * (1 + 0.06/12) ^ 12col
     this.points = [];
     this.points.push(this.startVal);
-    for ( let col = 1; col < this.totalCols; col++) {
-      // let val = this.startVal * (1 + 0.06/12) ^ 12 * col;
-      let val = this.startVal * Math.pow((1 + this.interest / 12), 12 * col) + (12 * col * .1);
-      // Math.round(this.startVal * Math.pow(ror, col - 1) * Math.pow(ror, col));
+    for ( let col = 1; col < this.totalCols + 2; col++) {
+      let val = this.startVal * Math.pow((1 + this.interest / 12), 12 * col);
       this.points.push(val);
     }
+    this.maxVal = 8 * this.startVal < 50 ? 50 : 8 * this.startVal;
+  }
 
-    this.maxVal = 8 * this.startVal; //this.points[this.points.length - 1];
+  calcOptGraphPoints() {
+
+    this.optPoints = [];
+    this.optPoints.push(this.startVal);
+    for ( let col = 1; col < this.totalCols + 2; col++) {
+      let val = this.startVal * Math.pow((1 + (this.interest * 1.15) / 12), 12 * col);
+      this.optPoints.push(val);
+    }
+
+  }
+
+  calcPessGraphPoints() {
+
+    this.pessPoints = [];
+    this.pessPoints.push(this.startVal);
+    for ( let col = 1; col < this.totalCols + 2; col++) {
+      let val = this.startVal * Math.pow((1 + (this.interest * 0.85) / 12), 12 * col);
+      this.pessPoints.push(val);
+    }
 
   }
 
@@ -169,21 +208,69 @@ export class ReadyGraphComponent implements OnInit {
     this.graphLine.graphics.beginStroke('#FFFFFF');
     this.graphLine.graphics.beginFill('#FFFFFF11')
     this.graphLine.graphics.moveTo(0, 0);
-    for ( let col = 0; col < this.totalCols; col++) {
+    for ( let col = 0; col < this.points.length; col++) {
       var x = Math.round((col * this.graphWidth / this.totalCols));
       var y = -this.points[col]/this.maxVal * this.graphHeight;//  Math.round(this.points[col] * (this.maxVal / this.totalRows));
       this.graphLine.graphics.lineTo(x, y);
     }
+
     this.graphLine.graphics.lineTo(x, 0);
     this.graphLine.graphics.lineTo(0, 0);
     this.graphLine.graphics.endStroke();
   }
 
+  plotOptGraphPoints() {
+    let incrVal = this.maxVal / this.totalRows;
+    if(this.graphLine_Opt == null) 
+    {
+      this.graphLine_Opt = new createjs.Shape();
+      this.graphContainer.addChild(this.graphLine_Opt);
+    }
+    this.graphLine_Opt.graphics.clear();
+    this.graphLine_Opt.graphics.setStrokeStyle(0);
+    this.graphLine_Opt.alpha = 1;
+    this.graphLine_Opt.graphics.beginStroke('#FFFFFF11');
+    this.graphLine_Opt.graphics.beginFill('#FFFFFF11')
+    this.graphLine_Opt.graphics.moveTo(0, 0);
+    for ( let col = 0; col < this.optPoints.length; col++) {
+      var x = Math.round((col * this.graphWidth / this.totalCols));
+      var y = -this.optPoints[col]/this.maxVal * this.graphHeight;//  Math.round(this.points[col] * (this.maxVal / this.totalRows));
+      this.graphLine_Opt.graphics.lineTo(x, y);
+    }
+
+    this.graphLine_Opt.graphics.lineTo(x, 0);
+    this.graphLine_Opt.graphics.lineTo(0, 0);
+    this.graphLine_Opt.graphics.endStroke();
+  }
+
+  plotPessGraphPoints() {
+    let incrVal = this.maxVal / this.totalRows;
+    if(this.graphLine_Pess == null) 
+    {
+      this.graphLine_Pess = new createjs.Shape();
+      this.graphContainer.addChild(this.graphLine_Pess);
+    }
+    this.graphLine_Pess.graphics.clear();
+    this.graphLine_Pess.graphics.setStrokeStyle(0);
+    this.graphLine_Pess.alpha = 1;
+    this.graphLine_Pess.graphics.beginStroke('#FFFFFF11');
+    this.graphLine_Pess.graphics.beginFill('#FFFFFF11')
+    this.graphLine_Pess.graphics.moveTo(0, 0);
+    for ( let col = 0; col < this.pessPoints.length; col++) {
+      var x = Math.round((col * this.graphWidth / this.totalCols));
+      var y = -this.pessPoints[col]/this.maxVal * this.graphHeight;
+      this.graphLine_Pess.graphics.lineTo(x, y);
+    }
+
+    this.graphLine_Pess.graphics.lineTo(x, 0);
+    this.graphLine_Pess.graphics.lineTo(0, 0);
+    this.graphLine_Pess.graphics.endStroke();
+  }
+
   onStageMouseMove(evt) {
     let scale = this.vertLine.x /  this.stageWidth;
     this.interest = .05 * scale;
-    this.calcGraphPoints();
-    this.plotGraphPoints();
+    this.drawGraphLines();
     this.vertLine.x = evt.stageX;
     //this.graphContainer.scaleX = this.graphContainer.scaleY = 1 + ( this.vertLine.x /  this.stageWidth);
   }
